c804f25699959569578325e302dd30eae418600d
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-12-02 19:20:41 +0100

    add/config-BBDD

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,11 @@
-const express = require("express")
+const express = require("express");
+require("dotenv").config();
 const app = express();
 const port = 3000;
+
+require('./config/db_sql');
+
+
 
 app.listen(port, () => {
   console.log(`Servidor backend -> http://localhost:${port}`)
