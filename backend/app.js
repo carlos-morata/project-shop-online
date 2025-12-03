d5ff40bf2c373ac48a6209027ad09ff3869d3ba2
CarlosMoratinos5 <moratinos.carlos5@gmail.com>
2025-12-03 12:01:07 +0100

    add/signup-login-logout

--- backend/app.js
+++ backend/app.js
@@ -4,8 +4,11 @@ const app = express();
 const port = 3000;
 
 require('./config/db_sql');
+app.use(express.json());
 
-
+// Importar Rutas de Usuarios
+const userRoutes = require('./routes/userRoutes');
+app.use('/', userRoutes);
 
 app.listen(port, () => {
   console.log(`Servidor backend -> http://localhost:${port}`)
